84f7cffe5f56bb080cba25147975e8b455780508
lucakorosec <korosecluca@gmail.com>
2021-06-08 21:11:37 +0200

    add fullscreen button to map & map settings

--- checkliste/main.js
+++ checkliste/main.js
@@ -1,8 +1,5 @@
 let baselayers = {
-    standard: L.tileLayer.provider("BasemapAT.basemap"),
-    grau: L.tileLayer.provider("BasemapAT.grau"),
     terrain: L.tileLayer.provider("BasemapAT.terrain"),
-    surface: L.tileLayer.provider("BasemapAT.surface"),
     highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
     wms: L.tileLayer.wms('https://maps.tirol.gv.at/externalcall.jsp?user=guest&project=tmap_master&view=sport_wintersport', {
         layers: 'PVECT\PLA_RO\SKJ_PL'
@@ -22,23 +19,21 @@ let overlays = {
 let map = L.map("map", {
     center: [47.267222, 11.392778],
     zoom: 9,
+    fullscreenControl: true, //leaflet fullscreen
     layers: [
-        baselayers.highdpi
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 })
 
 // Kartenhintergründe und Overlays zur Layer-Control hinzufügen
 let layerControl = L.control.layers({
-    "basemap.at Standard": baselayers.standard,
-    "basemap.at grau": baselayers.grau,
-    "basemap.at Relief": baselayers.terrain,
-    "basemap.at Oberfläche": baselayers.surface,
-    "basemap.at hochauflösend": baselayers.highdpi,
-    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay,
+    "STANDARD (basemap.at)": baselayers.highdpi,
+    "RELIEF (basemap.at)": baselayers.terrain,
+    //"basemap.at Orthofoto beschriftet": baselayers.ortho_overlay,
 }, {
     "GPX-Routen": overlays.routes,
-    "Wetter": baselayers.wms,
-    "Hütten": baselayers.wms,
+    "Daten: Wetterstationen": baselayers.wms,
+    //"Hütten": baselayers.wms,
 }).addTo(map);
 
 console.log(baselayers.wms);
